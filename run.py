6fb929e4c6477808a39fb996d61ed6273f66c7ed
Rongzhen Chen <rche@kth.se>
2015-01-01 20:34:51 +0100

    second commit

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import re
 
 
 fileINCAR='/home/rongzhen/workplace/python_coding/final/FIELS/INCAR'
@@ -17,26 +18,145 @@ fileRUNSCRIPT="/home/rongzhen/workplace/python_coding/final/FIELS/RUNSCRIPT"
 #D=['S', 'Se'];
 
 A=['Cu'];
-B=['Sc','Ti','Cu', 'Zn'];
-C=['Ti', 'Si'];
-D=['S', 'Se'];
+B=['Sc','Cu','Ti'];
+C=['Ti','Si'];
+D=['S'];
 
-phaseN=['FM','AFM1','AFM2']
+phaseN=['FM','AFM1','AFM2','AFM3']
 structureN=['KS', 'ST']
+incarN=['INCAR.pbe.relax1','INCAR.pbe.relax2','INCAR.pbe.relax3','INCAR.pbe.relax4','INCAR.pbe.relax5']
+ 
+#################### Function: replace many words###################
+def replace_words(text, word_dic):
+    rc = re.compile('|'.join(map(re.escape, word_dic)))
+    def translate(match):
+        return word_dic[match.group(0)]
+    return rc.sub(translate, text)
+
+#################### Function: generate all the POSCAR###################
+def genPOSCAR(eleA, eleB, eleC, eleD, structure, phase):
+
+    fileP=filePOSCAR+"/"+structure+"/"+phase
+    os.system("cp %s ." % fileP) 
+
+	# if A==B==Cu
+    if eleA == eleB:
+        strElementName= "      "+eleA+"   "+eleC+"   "+eleD
+        strElementNumber="    "+str(6)+"   "+str(2)+"   "+str(8)
+	# if B==C==Ti
+    elif eleB == eleC:
+        strElementName= "      "+eleA+"   "+eleB+"   "+eleD
+        strElementNumber="    "+str(4)+"   "+str(4)+"   "+str(8)
+	# normal case 
+    else:
+        strElementName= "      "+eleA+"   "+eleB+"   "+eleC+"   "+eleD
+        strElementNumber="    "+str(4)+"   "+str(2)+"   "+str(2)+"   "+str(8)
+    
+    fin = open(phase, "r")
+    strFin=fin.read() 
+    fin.close()
+    
+    word_dic = {
+    'elementsname': strElementName,
+    'elementsnumber': strElementNumber}
+
+    strFout=replace_words(strFin, word_dic)
+
+    fout = open("POSCAR", "w")
+    fout.write(strFout)
+    fout.close()
+
+    os.system("rm %s" % phase) 
 
 
-#def genPOSCAR(eleA, eleB, eleC, eleD):
 
+#################### Function: generate all the INCAR###################
+def genINCAR(eleA, eleB, eleC, eleD, phase):
 
+    dictU={'Sc': 4.4,
+           'Ti': 4.4,
+           'V' : 2.7,
+           'Cr': 3.5,
+           'Mn': 4.0,
+           'Fe': 4.6,
+           'Co': 5.0,
+           'Ni': 5.1,
+           'Cu': 4.0,
+           'Zn': 7.5}
 
-#def genINCAR(eleA, eleB, eleC, eleD):
+    fileI=fileINCAR+"/*"
+    os.system("cp %s ." % fileI)
 
+	# if A==B==Cu
+    if eleA == eleB:
+        strLdauJ= "LDAUJ      =   0.0   0.0    0.0"
 
+        if eleC == "Ti":
+            strLdauL= "LDAUL      =   2     2      0"
+            strLdauU= "LDAUU      =   4.0   4.4    0.0" 
+            if phase == 'FM':
+                strMagmom="6*0.0 4.0 4.0 8*0.0"
+            else:
+                strMagmom="6*0.0 4.0 -4.0 8*0.0"
+        else:
+            strLdauL= "LDAUL      =   2     0      0"
+            strLdauU= "LDAUU      =   4.0   0.0    0.0"
+            strMagmom="6*0.0 2*0.0 8*0.0"
+	# if B==C==Ti
+    elif eleB == eleC:
+        strLdauJ= "LDAUJ      =   0.0   0.0    0.0"
+        strLdauL= "LDAUL      =   2     2      0"
+        strLdauU= "LDAUU      =   4.0   4.4    0.0"
+        if phase == 'FM':
+            strMagmom="4*0.0 4*4.0 8*0.0"
+        else:
+            strMagmom="4*0.0 4.0 -4.0 4.0 -4.0 8*0.0"
+	# normal case 
+    else:
+        strLdauJ= "LDAUJ      =   0.0   0.0    0.0   0.0"
+
+        if eleC == "Ti":
+            strLdauL= "LDAUL      =   2     2      2     0"
+            strLdauU= "LDAUU      =   4.0   "+str(dictU[eleB])+"    "+str(dictU[eleC])+"   0.0"
+            if phase == 'FM':
+                strMagmom="4*0.0 4*4.0 8*0.0"
+            else:
+                strMagmom="4*0.0 4.0 -4.0 4.0 -4.0 8*0.0"
+        else:
+            strLdauL= "LDAUL      =   2     2      0     0"
+            strLdauU= "LDAUU      =   4.0   "+str(dictU[eleB])+"    0.0   0.0"
+            if phase == 'FM':
+                strMagmom="4*0.0 2*4.0 2*0.0 8*0.0"
+            else:
+                strMagmom="4*0.0 4.0 -4.0 2*0.0 8*0.0"
+
+    nList = len(incarN)
+    for i in range(0, nList-1):
+
+        incarF=fileINCAR+"/"+incarN[i]
+
+        fin = open(incarF, "r")
+        strFin=fin.read() 
+        fin.close()
+
+        os.system("rm %s" % incarF)
+
+        word_dic = {
+        'MAGMOMline': strMagmom,
+        'LDAUJline' : strLdauJ,
+        'LDAULline' : strLdauL,
+        'LDAUUline' : strLdauU}
+
+        strFout=replace_words(strFin, word_dic)
+
+        fout = open(incarF, "w")
+        fout.write(strFout)
+        fout.close()
 
 #################### Function: generate all the KPOINTS###################
 def genKPOINTS():
-   filek=fileKPOINTS+"/*"
-   os.system("cp %s ." % filek) 
+   fileK=fileKPOINTS+"/*"
+   os.system("cp %s ." % fileK) 
 
 
 
@@ -124,7 +244,7 @@ for eleA in A:
                 prevDir = os.getcwd()
 
                 dirName, labelName=genDir(eleA, eleB, eleC, eleD)
-                
+                print "compound name: ", dirName                
                 os.chdir(prevDir)
                 os.chdir(dirName)
 
@@ -137,6 +257,8 @@ for eleA in A:
                         genKPOINTS()
                         genRUN()
                         genPOTCAR(eleA, eleB, eleC, eleD)
+                        genPOSCAR(eleA, eleB, eleC, eleD, structure, phase)
+                        genINCAR(eleA, eleB, eleC, eleD, phase)
 
                         os.chdir(os.pardir)
                     os.chdir(os.pardir)
